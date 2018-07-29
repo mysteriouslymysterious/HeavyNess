1e370cee7199ea3b700c2691c2109b0ff042a99d
mysteriouslymysterious <31710172+mysteriouslymysterious@users.noreply.github.com>
2018-07-29 13:36:33 -0700

    Initial commit

---
+++ app/src/main/java/com/example/chris/all_the_other_way/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chris.all_the_other_way;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
